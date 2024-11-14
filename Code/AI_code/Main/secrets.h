fe7ccf390bb9b20e80c1eb607e84a2a8542b7b43
Samson Ajadalu <64048938+SamsonAjadalu@users.noreply.github.com>
2024-11-14 18:32:10 +0100

    Update secrets.h

--- Code/AI_code/Main/secrets.h
+++ Code/AI_code/Main/secrets.h
@@ -1,10 +1,10 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "Galaxy S23 C34F"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "22222225"	// replace MyPassword with your WiFi password
+#define SECRET_SSID ""        // replace with your WiFi network name
+#define SECRET_PASS ""        // replace with your WiFi password
 
-#define SECRET_CH_ID_WEATHER_STATION 12397	          	//MathWorks weather station
+#define SECRET_CH_ID_WEATHER_STATION 0              // replace with your MathWorks weather station ID
 
-#define SECRET_CH_ID_COUNTER 1984450 					//Test channel for counting
-#define SECRET_READ_APIKEY_COUNTER "EJ57MZUJEJYG3YGI"	//API Key for Test channel
+#define SECRET_CH_ID_COUNTER 0                      // replace with your Test channel ID
+#define SECRET_READ_APIKEY_COUNTER ""               // replace with your API Key for Test channel
