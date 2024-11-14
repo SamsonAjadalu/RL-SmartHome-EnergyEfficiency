4cb70577e8c9f150df680aa10e4e19f1b6cc9ed7
Samson Ajadalu <64048938+SamsonAjadalu@users.noreply.github.com>
2024-11-14 18:32:49 +0100

    Update secrets.h

--- Code/non_AI_code/Main.ino/secrets.h
+++ Code/non_AI_code/Main.ino/secrets.h
@@ -1,8 +1,10 @@
-// Use this file to store all of the private credentials 
+// file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "#####"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "#####"	// replace MyPassword with your WiFi password
+#define SECRET_SSID ""        // replace with your WiFi network name
+#define SECRET_PASS ""        // replace with your WiFi password
 
-#define SECRET_CH_ID_COUNTER #### 					//Test channel for counting
-#define SECRET_READ_APIKEY_COUNTER "#####"	//API Key for Test channel
+#define SECRET_CH_ID_WEATHER_STATION 0              // replace with your MathWorks weather station ID
+
+#define SECRET_CH_ID_COUNTER 0                      // replace with your Test channel ID
+#define SECRET_READ_APIKEY_COUNTER ""               // replace with your API Key for Test channel
